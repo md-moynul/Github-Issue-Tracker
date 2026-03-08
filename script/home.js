be26a990de85b98e19d3d67bb6ff19d90499bb01
moynul447 <mmmdmoynulislam@gmail.com>
2026-03-08 22:31:47 +0600

    add total funtion

--- script/home.js
+++ script/home.js
@@ -3,15 +3,17 @@ const openBtn = document.getElementById('openBtn');
 const closeBtn = document.getElementById('closeBtn');
 const cardContainer = document.getElementById('cardContainer');
 const spinner = document.getElementById('spinner');
-const modalTittle = document.getElementById('modalTittle');
-const modalStatus = document.getElementById('modalStatus');
-const modalName = document.getElementById('modalName');
-const modalNameB = document.getElementById('modalNameB');
-const modalDate = document.getElementById('modalDate');
-const modalDes = document.getElementById('modalDes');
-const modalPriority = document.getElementById('modalPriority');
+const total = document.getElementById('total');
+
+const modalDetails = document.getElementById('modalDetails');
+let all = [];
 let open = [];
 let close = [];
+
+function displayLabels(arr){
+    const HtmlEle = arr.map(el=> `<span class='bg-red-100 py-1.5 px-2 rounded-xl font-medium text-red-500 text-xs '>${el}</span>`)
+    return (HtmlEle.join(" "))
+}
 function loadingSpinner(status) {
     if (status === true) {
         spinner.classList.remove('hidden');
@@ -32,14 +34,14 @@ function toggle(id) {
     selected.classList.remove('bg-base-100');
     selected.classList.add('btn-primary');
     if (id === 'openBtn') {
-        loadingSpinner(true);
+        loadingSpinner(true); 
         displayData(open);
     } else if (id === 'closeBtn') {
         loadingSpinner(true)
         displayData(close);
     } else if (id === 'allBtn') {
         loadingSpinner(true);
-        loadData();
+        displayData(all);
     }
 }
 
@@ -49,6 +51,7 @@ async function loadData() {
     const data = await res.json();
 
     data.data.forEach(element => {
+        all.push(element)
         if (element.status === 'open') {
             open.push(element);
         } else {
@@ -63,11 +66,12 @@ function displayData(data) {
     cardContainer.innerHTML = '';
     data.forEach(element => {
 
+
         const newCard = document.createElement('div');
         if (element.status === 'open') {
             newCard.className = ('border-t-green-500 bg-base-100 rounded-xl  border-t-[3px]   shadow ')
         } else if (element.status === 'closed') {
-            newCard.className = ('border-t-primary bg-base-100 rounded-xl  border-t-[3px]   shadow ')
+            newCard.className = ('border-t-[#A855F7] bg-base-100 rounded-xl  border-t-[3px]   shadow ')
         }
         newCard.innerHTML = `
         <div onclick="modal('${element.id}')" class="">
@@ -81,15 +85,11 @@ function displayData(data) {
                                         ${element.priority}</p>
                                 </div>
                                 <div class="space-y-2">
-                                    <h2 class="font-semibold text-[14px] h-12">${element.title}</h2>
-                                    <p class="line-clamp-2 text-xs text-gray ">${element.description}</p>
+                                    <h2 class="font-semibold text-[16px] h-12">${element.title}</h2>
+                                    <p class="line-clamp-2 text-[14px] text-gray ">${element.description}</p>
                                 </div>
                                 <div class="flex gap-1">
-                                    <span
-                                        class="bg-red-100 py-1.5 px-2 rounded-xl font-medium text-red-500 text-xs">Bug</span>
-                                    <span
-                                        class="bg-red-100 py-1.5 px-2 rounded-xl font-medium text-red-500 text-xs">help
-                                        wanted</span>
+                                    ${displayLabels(element.labels)}
                                 </div>
 
                             </div>
@@ -107,6 +107,7 @@ function displayData(data) {
 
 
     })
+     total.innerText = cardContainer.children.length;
     loadingSpinner(false);
 
 }
@@ -114,13 +115,49 @@ async function modal(id) {
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
     const result = await res.json();
     const data = result.data;
-    modalTittle.innerText = data.title;
-    modalStatus.innerText = data.status;
-    modalName.innerText = data.author;
-    modalNameB.innerText = data.author;
-    modalDate.innerText = data.createdAt.slice(0, 10);
-    modalDes.innerText = data.description;
-    modalPriority.innerText = data.priority;
+    const newModal = document.createElement('div');
+    modalDetails.innerHTML = ''
+
+
+
+    newModal.className = 'space-y-6'
+    newModal.innerHTML = `
+        <h3 id="modalTittle" class="text-2xl font-bold">${data.title}</h3>
+                            <div class="flex items-center space-x-2">
+                                <p id="modalStatus"  class="px-4 py-2 text-xs text-white rounded-full">${data.status}
+                                </p>
+                                <span class="w-1 h-1 bg-[#64748B] rounded-full"></span>
+                                <p>
+                                    Opened by <span>${data.author}</span>
+                                </p>
+                                <span class="w-1 h-1 bg-[#64748B] rounded-full"></span>
+                                <p id="modalDate">
+                                    ${data.createdAt.slice(0, 10)}
+                                </p>
+                            </div>
+                            <p id="modalDes" class="line-clamp-2 text-[14px] text-gray ">${data.description}</p>
+                            <div class="flex">
+                                <div class="w-[50%] space-y-2.5">
+                                    <p class="text-gray ">
+                                        Assignee:  
+                                    </p>
+                                    <p id="modalNameB" class="font-semibold">
+                                    ${data.assignee  ? data.assignee :data.author }
+                                    </p>
+                                </div>
+                                <div class="space-y-2.5">
+                                    <p class="text-gray ">
+                                        Priority:
+                                    </p>
+                                    <p id="modalPriority" class="bg-red-500 text-white px-4 py-1.25 rounded-full ">
+                                    ${data.priority}
+                                    </p>
+                                </div>
+                            </div>
+                        
+    `
+    modalDetails.appendChild(newModal)
+    const modalStatus = document.getElementById('modalStatus');
     if (data.status === 'open') {
         modalStatus.classList.add('bg-green-500')
     } else if (data.status === 'closed') {
